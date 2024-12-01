1a740c2ec79c8efad83af130db3f5c192027bea8
Mark Pacheco <alignthestones@gmail.com>
2024-12-01 09:06:34 +0800

    change firebase import

--- js/userlogin.js
+++ js/userlogin.js
@@ -1,20 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyA8-gs42UP39ovLjtfQRUR7GI2PLuskFq4",
-  authDomain: "my-first-firebase-projec-1bd5f.firebaseapp.com",
-  projectId: "my-first-firebase-projec-1bd5f",
-  storageBucket: "my-first-firebase-projec-1bd5f.firebasestorage.app",
-  messagingSenderId: "848530380932",
-  appId: "1:848530380932:web:451f57523ec3e94b920ad1",
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+import app from "/iphowinProject/js/firebase.js";
 
 import {
   getAuth,
@@ -26,6 +10,7 @@ import {
 } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 
 const auth = getAuth(app);
+
 const getStartedBtn = document.getElementById("getStartedBtn");
 const mainLog = document.getElementById("mainLog");
 
